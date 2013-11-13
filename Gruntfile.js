ad91e8128ab2ef84cbe1f00d73b2bc2596fae58a
darul <darul75@gmail.com>
2013-11-13 20:49:57 +0100

    new name

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,7 +14,7 @@ module.exports = function(grunt) {
         preserveComments: 'some',
         report: 'min',
         banner: '/*! \n* @license <%= pkg.name %> - v<%= pkg.version %>\n' + 
-         '* (c) 2013 Julien VALERY https://github.com/darul75/jcc\n' +
+         '* (c) 2013 Julien VALERY https://github.com/darul75/jade-cache\n' +
          '* License: MIT \n*/\n'
         },         
         files: {'lib/jcc.js': ['src/jcc.js']
